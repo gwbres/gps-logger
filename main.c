46080516466f324d67f39178134523e9cb6a448f
Guillaume W. Bres <guillaume.bressaix@gmail.com>
2018-04-15 19:38:28 +0200

    added TX done logic

--- main.c
+++ main.c
@@ -29,9 +29,14 @@
 
 #define BUFSIZE	16
 
-volatile int ptr;
-volatile int bytes_to_send;
-char buffer[128];
+volatile int tx_ptr;
+volatile uint8_t tx_done;
+int bytes_to_send;
+char tx_buf[128];
+
+//volatile int rx_ptr;
+//int bytes_received;
+//char rx_buf[128];
 
 void init_gpio(void);
 void init_uart(void);
@@ -40,9 +45,19 @@ void init_platform(void);
 
 int main(int argc, char **argv){
 	init_platform();
-	ptr = 0;
-	strcpy(buffer, "Hello from MSP430\n");
-	bytes_to_send = strlen(buffer);
+
+	tx_ptr = 0;
+	tx_done = 0x00;
+	strcpy(tx_buf, "\r\nHello from MSP430\r\n");
+	bytes_to_send = strlen(tx_buf);
+	UC0IE |= UCA0TXIE; // TX IE
+	while(!tx_done);
+
+	tx_ptr = 0;
+	strcpy(tx_buf, "Line2 from MSP430\r\n");
+	bytes_to_send = strlen(tx_buf);
+	UC0IE |= UCA0TXIE; // TX IE
+	while(!tx_done); 
 
 	while(1);
 	return 0;
@@ -88,12 +103,12 @@ void init_uart(void){
 	UCA0MCTL = UCBRS2 + UCBRS0;
 	UCA0CTL1 &= ~UCSWRST;
 	//UC0IE |= UCA0RXIE; // RX IE
-	UC0IE |= UCA0TXIE; // TX IE
+	UC0IE &= ~UCA0TXIE; // disable TX ISR
 }
 
 #pragma vector = TIMER0_A0_VECTOR
 __interrupt void CCR0_ISR(void){
-	P1OUT = ~P1OUT;
+	P1OUT = ~LED_ISR;
 }
 
 #pragma vector = USCIAB0RX_VECTOR
@@ -111,10 +126,11 @@ __interrupt void USCI0RX_ISR(void){
 #pragma vector = USCIAB0TX_VECTOR
 __interrupt void USCI0TX_ISR(void){
 	P1OUT |= LED_UART;
-	UCA0TXBUF = buffer[ptr];
-	if (ptr == bytes_to_send-1){ // tx done
+	UCA0TXBUF = tx_buf[tx_ptr];
+	if (tx_ptr == bytes_to_send-1){ // tx done
 		UC0IE &= ~UCA0TXIE; // disable TX ISR
 		P1OUT &= ~LED_UART;
+		tx_done |= 0x01;
 	} else
-		ptr++;
+		tx_ptr++;
 }
