30500438805bfc78ba2ef666628151da4f02bd58
Guillaume W. Bres <guillaume.bressaix@gmail.com>
2018-04-22 13:17:12 +0200

    working on NMEA/KML/GPX tool suite; improved CLI

--- tools.py
+++ tools.py
@@ -2,6 +2,7 @@
 
 import sys
 import serial
+from datetime import date
 
 # PMTK module
 PMTK_SET_NMEA_UPDATE_100_MILLIHERTZ = "$PMTK220,10000*2F" 
@@ -75,58 +76,249 @@ def print_help():
 	string += "--nmea-rate\n\tSet GPS frames update rate [10Hz,5Hz,2Hz,1Hz,200mHz,100mHz]\n"
 
 	string += "\nNMEA data:\n"
-	string += "--cv-nmea-kml\n\tConverts .nmea file to .kml file (google earth, etc..)\n"
+	string += "--nmea-to-kml\n\tConverts .nmea file to .kml file (google earth, ..)\n"
+	string += "--nmea-to-gpx\n\tConverts .nmea file to .gpx file (Garmin, etc..)\n"
 
 	print(string)
 
-# Dumps all nmea frames into a .nmea file
-def dump_flash(tty, nmea):
-	print(write_cmd(PMKT_STOP_LOGGER))
+# Converts parsed coordinates to degrees/min/secs
+def coord_to_deg_min(coord):
+	#2503.6319 i.e ddmm.mmmm: 25°03.6319' = 25°03'37,914"
+	deg = coord.split(".")[0][0:1]
+	min = coord.split(".")[0][2:3]
+	secs = coord.split(".")[1]
+	return [deg,min,secs]
 
-	fd = open(nmea)
-	while (tty.readline()):
-		fd.write(line+"\n")
-	fd.close()
+def knots_to_kmph(knots):
+	mph = knots*1.15078
+	return mph*1.60934
+
+# GPGGA:
+# class descriptor for GPGGA NMEA frames
+class GPGGA:
+	def __init__(self, csv):
+		self.utc = "000000.00"
+		self.lat = None
+		self.long = None
+		self.alt = None
+		self.hdop = None
+
+		content = csv.split(",")[1:-1] # remove header and checksum
+		self.utc = content[0]
+		if ((content[1]) is not None): # missing if not FIXed
+			self.lat = [content[1],content[2]]
+			self.long = [content[3],content[4]]
+
+		# content[5]: '1' is GPS coordinates
+		# content[6]: number of sallites used
+	
+		# HDOP Horizontal dilution of Precision
+		if ((content[7] is not None)): # could be missing
+			self.hdop = content[7]
+		
+		if (content[8] is not None): # might be missing
+			self.alt = content[8] # content[9]='M' for meters
+
+	def __str__(self):
+		print("Time stamp: {:s}\n".format(self.get_utc()))
+		print("Latitude: {:f}\n".format(self.get_lat()))
+		print("Longitude: {:f}\n".format(self.get_long()))
+		print("Altitude: {:f}\n".format(self.get_alt()))
+		
+	def get_utc(self):
+		return self.utc
+	
+	def get_long(self):
+		return self.long
+
+	def get_lat(self):
+		return self.lat
+	
+	def get_alt(self):
+		return self.alt
+	
+	def get_hdop(self):
+		return self.hdop
+	
+	def _type(self):
+		return "GPGGA"
+
+class GPRMC:
+	def __init__(self, csv):
+		self.utc = "000000.00"
+		self.alt = None
+		self.lat = None
+		self.long = None
+		
+		content = csv.split(",")[1:-1] # remove header & checksum
+		self.utc = content[0]
+		if (content[1] == 'A'): # 'A':valid (proceed), 'V' non valid
+			self.lat = [content[2],content[3]]
+			self.long = [content[4],content[5]]
+			self.speed = knots_to_kmph(float(content[6]))
+			# content[7] route sur le fond
+			self.date = content[7]
+
+	def _type(self):
+		return "GPRMC"
+
+	def get_utc(self):
+		return self.utc
+
+	def get_lat(self):
+		return self.lat
+
+	def get_alt(self):
+		return "0"
+
+	def get_long(self):
+		return self.long
+
+	def get_speed(self):
+		return self.speed
+
+	def get_date(self):
+		return self.date
 
 # Converts .nmea file into .kml file
 # to use in most graphical GPS coordinates displayers
 # uses GPGGA frames only
-def convert_to_kml(nmea, kml):
-	extn = nmea.split(".")[-1]
-	fnmea = open(nmea, "r")
-	fkml = open(kml, "w")
+def nmea_to_kml(nmea, kml):
+	nmeafd = open(nmea, "r")
+	kmlfd = open(kml, "w")
 
-	for line in fnmea:
+	# initialize kml file
+	kmlfd.write('<?xml version="1.0" encoding="UTF-8"?>\n')
+	kmlfd.write('<kml xmlns="http://www.opengis.net/kml/2.2">\n')
+	kmlfd.write(' <Document>\n')
+
+	# set one style for polygons drawing
+	# declare a style to group waypoints
+	kmlfd.write('\t<Style id="greenPolyLine">\n')
+	kmlfd.write('\t\t<LineStyle>\n')
+	kmlfd.write('\t\t\t<color>7fff00ff</color>\n')
+	kmlfd.write('\t\t\t<width>4</width>\n')
+	kmlfd.write('\t\t</LineStyle>\n')
+	kmlfd.write('\t</Style>\n')
+
+	# all waypoints for this session will be 
+	# grouped together
+	"""
+	kmlfd.write("\t<Placemark>\n")
+	kmlfd.write("\t <styleUrl>#greenPolyLine</styleUrl>\n")
+	kmlfd.write("\t <LineString>\n")
+	kmlfd.write("\t <altitudeMode>absolute</altitudeMode>\n")
+	kmlfd.write("\t <coordinates>\n")
+	"""
+
+	for line in nmeafd:
+		GPS = None
 		line = line.strip() # cleanup
 		if not(line):
 			continue
 
-		# keep only GPGGA frames
-		if not(line.startswith("$GPGGA")):
+		if (line.startswith("$GPGGA")):
+			GPS = GPGGA(line)
+		elif (line.startswith("$GPRMC")):
+			GPS = GPRMC(line)
+
+		if (GPS is not None):
+			if (GPS.get_lat() == None):
+				continue
+
+			kmlfd.write('\t<Placemark>\n')
+
+			kmlfd.write('\t  <TimeStamp>\n')
+			if (GPS._type() == "GPRMC"):
+				d = GPS.get_date()
+				day = d[0:1]
+				month = d[2:3]
+				year = d[4:5]
+			else:
+				d = date.today()
+				year = str(d.year)
+				month = str(d.month)
+				day = str(d.day)
+
+			utc = GPS.get_utc()
+			h = utc.split(".")[0][0:1]
+			m = utc.split(".")[0][2:3]
+			s = utc.split(".")[0][4:5]
+
+			kmlfd.write('\t  <when>20{:s}-{:s}-{:s}T{:s}:{:s}:{:s}Z</when>\n'.format(
+				year,month,day,h,m,s	
+			))
+			kmlfd.write('\t  </TimeStamp>\n')
+
+			kmlfd.write('\t  <styleUrl>#greenPolyLine</styleUrl>\n')
+
+			kmlfd.write('\t  <Point>\n')
+			kmlfd.write('\t\t<coordinates>{:s},{:s},{:s}</coordinates>\n'.format(
+				GPS.get_lat()[0],GPS.get_long()[0],GPS.get_alt()
+			))
+			kmlfd.write('\t  </Point>\n')
+			kmlfd.write('\t</Placemark>\n')
+
+	# finalize kml file 
+	kmlfd.write(' </Document>\n')
+	kmlfd.write('</kml>\n')
+
+	nmeafd.close()
+	kmlfd.close()
+	print("NMEA file {:s} content converted to KML in {:s}".format(nmea,kml))
+
+def nmea_to_gpx(nmea, gpx):
+	nmeafd = open(nmea,"r")
+	gpxfd = open(gpx,"w")
+
+	gpxfd.write('<?xml version="1.0" encoding="UTF-8" standalone="no" ?>\n')
+	gpxfd.write('<gpx xmlns="http://www.topografix.com/GPX/1/1" creator="byHand" version="1.1"\n')
+	gpxfd.write('  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"\n') 
+	gpxfd.write('  xsi:schemaLocation="http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd">\n')
+
+	for line in nmeafd:
+		GPS = None
+		line = line.strip() # cleanup
+		if not(line):
 			continue
+		
+		if (line.startswith("$GPGGA")):
+			GPS = GPGGA(line)
+		elif (line.startswith("$GPRMC")):
+			GPS = GPRMC(line) 
 
-		NMEA = GPGGA(line)
+		if (GPS is not None):
+			gpxfd.write('<wpt lat="{:s}" lon="{:s}">\n'.format(GPS.get_lat()[0],GPS.get_long()[0]))
+			gpxfd.write('\t<name>waypoint</name>\n')
+			gpxfd.write('\t<ele>{:s}</ele>\n'.format(GPS.get_alt()))
+			if (GPS._type() == "GPRMC"):
+				date = GPS.get_date()
+				day = date[0:1]
+				month = date[2:3]
+				year = date[4:5]
 
-		kml_content = "<when>T%(time_string)sZ</when>" % result
-		kml_content = "<gx:coord>%(longitude)s %(latitude)s %(altitude)s</gx:coord>\n" % result
-		kml.write(kml_content)
+				utc = GPS.get_utc()
+				h = utc.split(".")[0][0:1]
+				m = utc.split(".")[0][2:3]
+				s = utc.split(".")[0][4:5]
 
-	fnmea.close()
-	fkml.close()
-	print("NMEA file {:s} content converted into KML in {:s}\n".format(nmea,kml))
+				gpxfd.write('\t<time>20{:s}-{:s}-16T{:s}:{:s}:{:s}Z</time>\n'
+					.format(year,month,day,h,m,s)
+				)
 
-def main(argv):
-	argv = argv[1:]
+			gpxfd.write('</wpt>\n')
 
-	if (len(argv) == 0):
-		print_help()
-		return -1
-
+	gpxfd.write('</gpx>')
+	nmeafd.close()
+	gpxfd.close()
+	print("NMEA file {:s} content converted to GPX in {:s}".format(nmea,gpx))
+	
+# opens serial port
+def open_serial():
 	ser = serial.Serial(argv[0])
 	ser.open()
 	ser.flushInput()
 	ser.flushOutput()
-
 	# setup
 	ser.bytesize = serial.EIGHTBITS
 	ser.parity = serial.PARITY_NONE
@@ -136,24 +328,33 @@ def main(argv):
 	ser.rtscts = False
 	ser.dsrdtr = False
 	ser.writeTimeout = 2
+	return ser
 
-	for flag in argv[1:]:
+def main(argv):
+	argv = argv[1:]
+
+	if (len(argv) == 0):
+		print_help()
+		return -1
+
+	for flag in argv:
+		print(flag)
 		
 		if flag == "--start-logging":
-			print(write_cmd(PMKT_START_LOGGER))
+			print(write_cmd(open_serial(), PMKT_START_LOGGER))
 		
 		elif flag == "--stop-logging":
-			print(write_cmd(PMKT_STOP_LOGGER))
+			ser = open_serial()
+			print(write_cmd(open_serial(), PMKT_STOP_LOGGER))
 
 		elif flag == "--erase-flash":
 			c = input("Are you sure? [Y/N]")
 			if (c == "Y"):
-				print(write_cmd(PMKT_FLASH))
-
-		elif flag == "--dump":
-			dump_flash(ser)
+				ser = open_serial()
+				print(write_cmd(open_serial(), PMKT_FLASH))
 
 		elif flag == "--baud":
+			ser = open_serial()
 			b = input("Select baud [9600;57600]")
 
 		elif flag == "--rate":
@@ -176,7 +377,15 @@ def main(argv):
 				cmd = PMTK_SET_NMEA_UPDATE_1HZ
 				print("Rate {:s} is not supported")
 				print("Switching back to 1 Hz default rate")
-			print(ser.write_cmd(cmd))
+			print(write_cmd(open_serial(), cmd))
+
+		elif flag == "--nmea-to-kml":
+			fp = input("Set input file path..\n")
+			nmea_to_kml(fp, fp.split(".")[0]+".kml")
+
+		elif flag == "--nmea-to-gpx":
+			fp = input("Set input file path..\n")
+			nmea_to_gpx(fp, fp.split(".")[0]+".gpx")
 		
 		elif flag == "--help":
 			print_help()
@@ -184,7 +393,5 @@ def main(argv):
 			print_help()
 		elif flag == "help":
 			print_help()
-
-	ser.close()
 
 main(sys.argv)
